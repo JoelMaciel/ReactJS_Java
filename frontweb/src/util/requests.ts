c5cbc16fdb349da3deaafc470611c439364a003a
JoelMaciel <soldado25461@gmail.com>
2021-10-04 14:04:58 -0300

    Check user authenticatated

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,6 +1,15 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import history from './history';
+import jwtDecode from 'jwt-decode';
+
+type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+
+type TokenData = {
+    exp: number,
+    user_name: "maria@gmail.com",
+    authorities : Role[];
+}
 
 type LoginResponse = {
   access_token: string;
@@ -73,3 +82,18 @@ axios.interceptors.response.use(function (response) {
   }
   return Promise.reject(error);
 });
+
+export const getTokenData = () : TokenData | undefined =>{
+  
+  try {
+    return jwtDecode(getAuthData().access_token) as TokenData; 
+  } catch (error) {
+    return  undefined;
+  }
+}
+
+export const isAuthenticated = () : boolean =>{
+  const tokenData = getTokenData();
+
+  return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
+}
