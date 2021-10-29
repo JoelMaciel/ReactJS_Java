d36aafdb0f3ba054c1dc99ccd667cb5959ee34dc
Joel Maciel <soldado25461@gmail.com>
2021-10-29 13:16:46 -0300

    Login, Logout logic

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -5,9 +5,9 @@ import jwtDecode from 'jwt-decode';
 
 type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
 
-type TokenData = {
+export type TokenData = {
     exp: number,
-    user_name: "maria@gmail.com",
+    user_name: string,
     authorities : Role[];
 }
 
@@ -64,6 +64,10 @@ export const getAuthData = () =>{
     const str = localStorage.getItem(tokenKey) ?? "{}";
     return JSON.parse(str) as LoginResponse;
 };
+
+export const removeAuthData = () => {
+  localStorage.removeItem(tokenKey);
+}
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
